6741fabb522b645ed0464263704cd2b121a6560a
Harshwardhan Patil <126240589+HarshwardhanPatil07@users.noreply.github.com>
2024-10-13 12:47:47 +0000

    Website ready to launch  for Hactoberfest2024

---
+++ Frontend/src/Components/HeroSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../CSS/HeroSection.css';
+
+const HeroSection = () => {
+          return (
+                    <>
+
+                              <section className="hero">
+                                        <div className="hero-content">
+                                                  <h1>Compiler-Visulization</h1>
+                                        </div>
+                              </section>
+
+                    </>
+          )
+}
+
+export default HeroSection
